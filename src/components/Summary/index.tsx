f88bd5cd29cb132d74f430de42649fb5912ae514
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-08-27 09:32:12 -0300

    feat: adicionando hook useSummary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
 import { 
   ArrowCircleDown, 
   ArrowCircleUp, 
   CurrencyDollar 
 } from "phosphor-react";
 
-import { TransactionContext } from "../../contexts/TransactionsContext";
+import { useSummary } from "../../hooks/useSummaryt";
 import { priceFormatter } from "../../utils/formatter";
 import { 
   SummaryContainer, 
@@ -13,27 +12,7 @@ import {
 } from "./styles";
 
 export function Summary(){
-
-  const { transactions } = useContext(TransactionContext)
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    }, 
-    {
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
